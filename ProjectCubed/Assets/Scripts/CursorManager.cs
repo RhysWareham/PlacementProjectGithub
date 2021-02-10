efdb3a5eec4709aa65e2792f14a426bfaffc6f2e
Joshua Shelley-Sak <s1801608@connect.glos.ac.uk>
2021-02-10 23:51:57 +0000

    Implemented Beat Meter as Mouse Cursor
    Follow this tutorial for future implementations of changing the animation of the cursor depending on what has happeed.
    
    Miss time the click = Flash Red.

---
+++ ProjectCubed/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D[] cursorTextureArray;
+    [SerializeField] private int frameCount;
+    [SerializeField] private float frameRate;
+    private int currentFrame;
+    private float frameTimer;
+
+
+    private void Start()
+    {
+        Cursor.SetCursor(cursorTextureArray[0], new Vector2(16, 16), CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        frameTimer -= Time.deltaTime;
+        if(frameTimer <= 0f)
+        {
+            frameTimer += frameRate;
+            currentFrame = (currentFrame + 1) % frameCount;
+            Cursor.SetCursor(cursorTextureArray[currentFrame], new Vector2(16, 16), CursorMode.Auto);
+        }
+    }
+
+}
