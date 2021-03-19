a79f26a8629c67b01fd3a8cfb58ae014c5115944
Joshua Shelley-Sak <s1801608@connect.glos.ac.uk>
2021-03-19 18:53:02 +0000

    Intro Implemented
    - 4 Videos that are played each time the one before finishes and the player clicks the mouse button.
    - Just need to add the scene that it leads into (read the script)

---
+++ ProjectCubed/Assets/Intro/IntroScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+
+public class IntroScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private VideoClip[] videoClips;
+    [SerializeField] private VideoPlayer videoPlayer;
+    private int currentClip = 0;
+
+    void Start()
+    {
+        currentClip = 0;
+        videoPlayer.clip = videoClips[currentClip];
+    }
+
+    void FixedUpdate()
+    {
+        if(currentClip > 3)
+        {
+            print("load scene");
+            //SceneManager.LoadScene(""); // <-- Just add the scenename that the intro leads into
+        }
+        else
+        {
+            if (Mouse.current.leftButton.isPressed && videoPlayer.isPaused == true) // Could maybe add an indicator on screen when the player can click.
+            {          
+                print("mouse clicked");
+                currentClip++;
+                print("new currentClip int = " + currentClip);
+                videoPlayer.clip = videoClips[currentClip];
+                videoPlayer.Play();
+            }
+        }
+    }
+}
